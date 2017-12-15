fc56d0510e0e6009b822351f39af34ea335c8140
jader_tao <jader_tao@qq.com>
2017-12-16 00:11:04 +0800

    1.12(B)

--- 1/1.2.2/1.12.scm
+++ 1/1.2.2/1.12.scm
@@ -1,18 +1,24 @@
 ; 非正常位置输出 0
 ; 递归版本
-(define (pascal m n)
+(define (pascal row col)
   (cond 
-    ((or (< n 0) (= n 0) (< m 0) (= m 0) (< m n)) 0)
-    ((= n 1) 1)
-    ((= n m) 1)
-    (else (+ (pascal (- m 1) (- n 1))
-             (pascal (- m 1) n)))))
+    ((or (< col 0) (< row 0) (< row col)) 0)
+    ((or (= col 0) (= col row)) 1)
+    (else (+ (pascal (- row 1) (- col 1))
+             (pascal (- row 1) col)))))
 
-; 递归版本
-(define (pascal-i m n)
-  (cond
-    ((or (< n 0) (= n 0) (< m 0) (= m 0) (< m n)) 0)
-    ((= n 1) 1)
-    ((= n m) 1)
-    (else (pascal-iterator ))
-    ))
+; 递归版本 (新公式)
+(define (factorial n)
+    (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+    (if (> counter max-count)
+        product
+        (fact-iter (* counter product)
+                   (+ counter 1)
+                   max-count)))
+
+(define (pascal-i row col)
+    (/ (factorial row)
+       (* (factorial col)
+          (factorial (- row col)))))
